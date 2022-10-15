b6aef1fc2e585e4106128ca88dd28f2fbd75d200
AleksandrMos <94990795+AleksandrMos@users.noreply.github.com>
2022-10-15 11:55:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+Задача №2
+
+Console.WriteLine ("Введите 1 число");
+int numder_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите 2 число");
+int numder_2 = Convert.ToInt32(Console.ReadLine());
+if(numder_1>numder_2)
+{
+    Console.WriteLine("Наибольшее число");
+    Console.WriteLine(numder_1);
+}
+else
+{
+  Console.WriteLine("Наибольшее число");
+  Console.WriteLine(numder_2);
+}
+*/
+
+/*Задача 4
+
+Console.WriteLine ("Введите 1 число");
+int numder_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите 2 число");
+int numder_2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите 3 число");
+int numder_3 = Convert.ToInt32(Console.ReadLine());
+
+if(numder_1>numder_2 & numder_1>numder_3)
+{
+    Console.WriteLine("Наибольшее число");
+    Console.WriteLine(numder_1);
+}
+
+if(numder_2>numder_1 & numder_2>numder_3)
+{
+    Console.WriteLine("Наибольшее число");
+    Console.WriteLine(numder_2);
+}
+else
+{
+  Console.WriteLine("Наибольшее число");
+  Console.WriteLine(numder_3);
+}
+*/
+
+/*Задача №6
+
+Console.WriteLine ("Введите число");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+int number_2 =number_1%2;
+
+if (number_2>0)
+{
+    Console.WriteLine("Число нечетное");
+}
+else
+{
+   Console.WriteLine("Число четное"); 
+}
+*/
+/*Задача №8
+
+Console.WriteLine ("Введите число");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+int Num_2 =0;
+while (Num_2<number_1)
+if (Num_2+2<number_1)
+{
+    Num_2+=2;
+    Console.Write(Num_2);
+}
+else
+{
+ Num_2+=2;   
+}
+*/
