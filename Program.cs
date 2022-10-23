99a316df19d806877a03ffce85994e362a95c923
AleksandrMos <94990795+AleksandrMos@users.noreply.github.com>
2022-10-23 11:39:40 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,56 +1,59 @@
-/*
-Задача №10
+﻿/*
 
-Console.WriteLine("Введите трёхзначное число");
-int number_1 = Convert.ToInt32(Console.ReadLine());
-int number_2 = (number_1%100)/10;
-
-Console.WriteLine(number_2);
-*/
-
- /*
- Задача №13
-
- Console.WriteLine("Введите число");
-int number_1 = Convert.ToInt32(Console.ReadLine());
-int number_2 = 0;
-
-if (number_1<100)
-{
-   Console.WriteLine("Третьей цыфры нет");
-}
-if (number_1<1000 & number_1>100)
-{
-   number_2 = number_1%100%10;
-   Console.WriteLine(number_2);
-}
-if (number_1<10000 & number_1>1000)
-{
-   number_2 = ((number_1%1000)%100/10);
-   Console.WriteLine(number_2);
-}
- if (number_1<100000 & number_1>10000)
-{
-   number_2 = ((number_1%10000)%1000/100);
-   Console.WriteLine(number_2);
-}
-*/
-
-/*Задача №15
+Задача №19
 
 Console.WriteLine("Введите число");
-int number_1 = Convert.ToInt32(Console.ReadLine());
+int num = int.Parse(Console.ReadLine());
+int num2 = num;
+int big = 0;
+int rev = 0;
 
-if (number_1==6 || number_1==7)
+while (num > 0)
 {
-   Console.WriteLine("Да, день выходной");
+    big = num % 10;
+    rev = rev * 10 + big;
+    num = (num / 10);
 }
-if (number_1<6 & number_1>0)
+if (num2==rev)
 {
-   Console.WriteLine("Нет, работайте господа");
+    Console.WriteLine("Число является полиндромом");
 }
-if (number_1<=0 || number_1>7)
+else
 {
-   Console.WriteLine("Такого дня недели нет!");
+   Console.WriteLine("Число не является полиндромом"); 
 }
- */
+*/
+ /*
+  Задача №21
+ Console.WriteLine("Введите координаты X1");
+int X1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Y1");
+int Y1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Z1");
+int Z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты X2");
+int X2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Y2");
+int Y2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Z2");
+int Z2 = int.Parse(Console.ReadLine());
+
+double D = Math.Sqrt(Math.Pow(X2-X1, 2)+Math.Pow(Y2-Y1, 2)+Math.Pow(Z2-Z1,2));
+Console.WriteLine($"Расстояние между двумя точками в пространстве равно {D}");
+*/
+/*
+Задача №23
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+for(int i=1;i<=num;i++)
+{
+  double num2 = Math.Pow(i,3);
+  Console.WriteLine(num2);
+}
+*/
